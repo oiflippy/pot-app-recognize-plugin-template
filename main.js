1ddb44f71350767bebbf97d5d63408c2acafc2d1
oiflippy <92901799+oiflippy@users.noreply.github.com>
2025-01-12 15:58:34 +0800

    再次测试

--- main.js
+++ main.js
@@ -1,49 +1,75 @@
 async function recognize(base64, lang, options) {
     const { config, utils } = options;
-    const { tauriFetch } = utils;
-    let { apikey, engine } = config;
-    base64 = `data:image/png;base64,${base64}`;
+    const { tauriFetch: fetch } = utils;
+    let { model = "gpt-4o", apiKey, requestPath, customPrompt } = config;
 
-    if (apikey === undefined || apikey.length === 0) {
-        throw "apikey not found";
+    // 处理请求路径
+    if (!requestPath) {
+        requestPath = "https://api.openai.com";
     }
-    if (engine === undefined || engine.length === 0) {
-        engine = "1";
+    if (!/https?:\/\/.+/.test(requestPath)) {
+        requestPath = `https://${requestPath}`;
+    }
+    if (requestPath.endsWith('/')) {
+        requestPath = requestPath.slice(0, -1);
+    }
+    if (!requestPath.endsWith('/chat/completions')) {
+        requestPath += '/v1/chat/completions';
     }
 
-    let res = await tauriFetch('https://api.ocr.space/parse/image', {
-        method: "POST",
-        header: {
-            apikey,
-            "content-type": "application/x-www-form-urlencoded"
-        },
-        body: {
-            type: "Form",
-            payload: {
-                base64Image: base64,
-                OCREngine: engine,
-                language: lang
+    // 处理自定义 Prompt
+    if (!customPrompt) {
+        customPrompt = "Just recognize the text in the image. Do not offer unnecessary explanations.";
+    } else {
+        customPrompt = customPrompt.replaceAll("$lang", lang);
+    }
+
+    const headers = {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${apiKey}`
+    }
+
+    const body = {
+        model,
+        messages: [
+            {
+                "role": "system",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": customPrompt
+                    }
+                ],
+            },
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": `data:image/png;base64,${base64}`,
+                            "detail": "high"
+                        },
+                    },
+                ],
             }
+        ],
+    }
+
+    let res = await fetch(requestPath, {
+        method: 'POST',
+        url: requestPath,
+        headers: headers,
+        body: {
+            type: "Json",
+            payload: body
         }
-    })
+    });
 
     if (res.ok) {
-        const { result } = res.data;
-        const { ErrorMessage, ParsedResults } = result;
-        if (ErrorMessage) {
-            throw ErrorMessage;
-        }
-        if (ParsedResults) {
-            let target = "";
-            for (let i in ParsedResults) {
-                const { ParsedText } = i;
-                target += ParsedText;
-            }
-            return target;
-        } else {
-            throw JSON.stringify(result);
-        }
+        let result = res.data;
+        return result.choices[0].message.content;
     } else {
-        throw JSON.stringify(res);
+        throw `Http Request Error\nHttp Status: ${res.status}\n${JSON.stringify(res.data)}`;
     }
 }
