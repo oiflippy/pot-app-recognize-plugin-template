8560345434b7a44313ce0a56dd29ef91dc312646
oiflippy <92901799+oiflippy@users.noreply.github.com>
2025-03-29 22:13:26 +0800

    移除自定义提示词

--- main.js
+++ main.js
@@ -19,14 +19,7 @@ async function recognize(base64, lang, options) {
         requestPath = requestPath.slice(0, -1);
     }
 
-    // 处理自定义 Prompt
-    if (!customPrompt) {
-        customPrompt = model.includes("minimax") 
-            ? "MM智能助理是一款由MiniMax自研的，没有调用其他产品的接口的大型语言模型。MiniMax是一家中国科技公司，一直致力于进行大模型相关的研究。"
-            : "只输出文字识别内容，不要说'这张照片展示了'，不要说'有什么我可以帮助你的吗？无论是问题解答、信息查询还是其他任何事情，请随时告诉我。'";
-    } else {
-        customPrompt = customPrompt.replaceAll("$lang", lang);
-    }
+    const systemPrompt = "只输出文字识别内容，不要说'这张照片展示了'，不要说'有什么我可以帮助你的吗？无论是问题解答、信息查询还是其他任何事情，请随时告诉我'";
 
     const headers = {
         'Content-Type': 'application/json',
@@ -38,8 +31,7 @@ async function recognize(base64, lang, options) {
         messages: [
             {
                 "role": "system",
-                "content": customPrompt,
-                "name": model.includes("minimax") ? "MM智能助理" : undefined
+                "content": systemPrompt
             },
             {
                 "role": "user",
